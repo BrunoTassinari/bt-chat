1d3f5e8d05d52ce989e3eca418de6038dcc87470
Bruno Tassinari <bruno.fagundes80@gmail.com>
2024-03-20 21:57:17 -0300

    feat: db configuratuion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-const Home = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
-);
+import { db } from '@/lib/db';
+
+const Home = async () => {
+  await db.set('hello', 'world');
+
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
 
 export default Home;
