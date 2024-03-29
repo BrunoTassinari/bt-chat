13e3b4b9dfeac639383790f5eebb262a1e80ff72
Bruno Tassinari <bruno.fagundes80@gmail.com>
2024-03-28 22:12:52 -0300

    feat: icon component

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,10 +1,11 @@
 import { getServerSession } from 'next-auth';
+import { notFound } from 'next/navigation';
 import { authOptions } from '@/lib/auth';
 
 export const getCurrentSession = async () => {
   const session = await getServerSession(authOptions);
 
-  if (!session) return new Response('Unauthorized', { status: 401 });
+  if (!session) notFound();
 
   return session;
 };
