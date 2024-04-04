24da0932c78875ca83b9e0baead793b719e503e3
Bruno Tassinari <bruno.fagundes80@gmail.com>
2024-04-03 21:48:19 -0300

    feat: dashboard layout

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,13 +5,13 @@ import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
   `active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium 
-  transition-color focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 
+  transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 
   disabled:opacity-50 disabled:cursor-not-allowed disabled:pointer-events-none`,
   {
     variants: {
       variant: {
         default: `bg-slate-900 text-white hover:bg-slate-800`,
-        ghost: `bg-transparent text-slate-900 hover:bg-islate-200`,
+        ghost: `bg-transparent text-slatree-900 s hover:bg-islate-200`,
       },
       size: {
         default: 'h-10 py-2 px-4',
